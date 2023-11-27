8dcec1f6155a25b7a90d3ae4f81db40a5493dfd4
koi <yuxintao6@163.com>
2023-11-27 18:37:20 +0800

    👻feat: 更改菜单折叠图标

--- src/layouts/components/Header/components/Collapse.vue
+++ src/layouts/components/Header/components/Collapse.vue
@@ -1,7 +1,7 @@
 <template>
   <el-icon class="collapse-icon koi-icon" :size="20" @click="changeCollapseIcon">
-    <SvgIcon name="koi-menu-left" width="20px" height="20px" v-if="globalStore.isCollapse"></SvgIcon>
-    <SvgIcon name="koi-menu-right" width="20px" height="20px" v-if="!globalStore.isCollapse"></SvgIcon>
+    <SvgIcon name="koi-menu-left" width="20px" height="20px" v-if="!globalStore.isCollapse"></SvgIcon>
+    <SvgIcon name="koi-menu-right" width="20px" height="20px" v-if="globalStore.isCollapse"></SvgIcon>
     <!-- <component :is="globalStore.isCollapse ? 'Expand' : 'Fold'"></component> -->
   </el-icon>
 </template>
