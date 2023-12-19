c317aa724f760db5f39b6fbc63fe1e98f9960bca
koi <yuxintao6@163.com>
2023-12-19 19:17:52 +0800

    👻feat：hooks封装字典

--- src/hooks/dicts/index.ts
+++ src/hooks/dicts/index.ts
@@ -1,13 +1,17 @@
-import { reactive, onMounted } from 'vue'
+import { reactive, onMounted } from "vue";
 import { listDataByType } from "@/api/system/dict/data/index.ts";
 
 export function useKoiDict(dictType: Array<string>) {
-  let koiDicts: any = reactive({})
+  let koiDicts: any = reactive({});
   onMounted(async () => {
-    for (const type of dictType) {
-      const res: any = await listDataByType(type);
-      koiDicts[type] = res.data;
+    if (dictType.length > 0) {
+      for (const type of dictType) {
+        const res: any = await listDataByType(type);
+        if (res.data != null) {
+          koiDicts[type] = res.data;
+        }
+      }
     }
-  })
-  return { koiDicts }
+  });
+  return { koiDicts };
 }
